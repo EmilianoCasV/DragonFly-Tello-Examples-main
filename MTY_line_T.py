2d5dfe02bb7c092be5b7bd6852d13a315cd9e1df
EmilianoCasV <emilianocastromessi@gmail.com>
2026-04-01 17:54:22 -0600

    Resto

--- MTY_line_T.py
+++ MTY_line_T.py
@@ -1,4 +1,3 @@
-# Codigo Torneo MTY (CTCHI)  detector linea y color
 
 import cv2
 import numpy as np
@@ -37,7 +36,7 @@ color_detectado = ""
 
 
 ##------------------PID Controller---------------##
-yaw_pid = PID(0.05, 0.000021, 2, setpoint=0)
+yaw_pid = PID(1.3, 0.000021, 1, setpoint=0)
 #x_pid_line=PID(0.000002, 0.000021, 0.1, setpoint=0) ##jashdjasjdhas
 x_pid_line=PID(0.2, 0.0000001, 0.1, setpoint=0) ##jashdjasjdhas
  
@@ -52,12 +51,12 @@ state = 0
 
 
 lower_b = np.array([0])
-upper_b = np.array([45])
+upper_b = np.array([60])
 
 angle=0
 dron.send_rc_control(0,0,0,0)
-#dron.takeoff()
-#dron.move_forward(30)
+dron.takeoff()
+dron.move_forward(30)
 dron.streamon()
 dron.RESOLUTION_480P
 
@@ -113,12 +112,16 @@ while True:
     if state_color==False:
         if (pixel_azul or pixel_rojo) > 70000:
             if color_detectado == "Rojo":
-                dron.move_forward(100)
-                dron.rotate_clockwise(45)
+                dron.move_up(50)
+                dron.move_forward(260)
+                dron.rotate_clockwise(55)
+                dron.move_down(60)
                 state_color = True
             if color_detectado == "Azul":
-                dron.move_forward(100)
-                dron.rotate_counter_clockwise(45)
+                dron.move_up(50)
+                dron.move_forward(260)
+                dron.rotate_counter_clockwise(55)
+                dron.move_down(60)
                 state_color = True
 
 
@@ -143,8 +146,8 @@ while True:
         cv2.circle(base,(xm,(y1 + y2)//2), 5, (255,0,0), -1)
         cv2.circle(base,(x_center,y_center),5,(0,0,255),-1)
 
-        if -25 <= error_c <= 25:
-            forward_speed = 12  #14 Nuevo Cambio (Tejada): Solo crear la variable ya usada en la parte de abajo. Antes solo era el número, ahora tiene nombre de variable.
+        if -15 <= error_c <= 15:
+            forward_speed = 10  #14 Nuevo Cambio (Tejada): Solo crear la variable ya usada en la parte de abajo. Antes solo era el número, ahora tiene nombre de variable.
             dron.send_rc_control(-int(x_pid_line((error_c/2), now_time - last_time)), forward_speed, 0,0) # Nuevo Cambio (Tejada): Aquí se aplica la variable forward_speed
 
         else:
@@ -154,7 +157,7 @@ while True:
              
               # Nuevo Cambio (Tejada): Nuevas variables para control de velocidad hacia adelante y el ángulo máximo
                 max_speed = 10
-                min_speed = 0
+                min_speed = 1
                 max_angle = 90
 
 
@@ -184,7 +187,7 @@ while True:
     # print(frame.shape)    # shape do be (480, 640, 3)
     cv2.putText(base,f"Bateria: {dron.get_battery()}",(30,460),cv2.FONT_HERSHEY_TRIPLEX,0.8,(255,0,0),1,cv2.LINE_AA)
     cv2.putText(base, f"Color: {color_detectado}", (30,80), cv2.FONT_HERSHEY_PLAIN, 1.5, (255 ,255 ,255), 2, cv2.LINE_AA)
-    cv2.putText(base, f"No.Pizeles: {pixel_azul}", (30,100), cv2.FONT_HERSHEY_PLAIN, 1.5, (255 ,255 ,255), 2, cv2.LINE_AA)
+    cv2.putText(base, f"No.Pixeles: {pixel_azul}", (30,100), cv2.FONT_HERSHEY_PLAIN, 1.5, (255 ,255 ,255), 2, cv2.LINE_AA)
 
 
     #cv2.imshow('frame', frame)
